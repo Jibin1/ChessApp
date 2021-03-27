39ba3be755da77098f7f52b578af3a7f04db8c3d
Jibin1 <thomasjibin9@gmail.com>
2021-03-27 12:52:23 -0400

    Added custom Button

---
+++ ChessApp/ChessButton.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace ChessApp
+{
+    public class ChessButton : Button
+    {
+        public int row { get; set; }
+        public int col { get; set; }
+        //private Color textColor;
+        //private Color backColor; 
+
+        public ChessButton(int row, int col)
+        {
+            this.row = row;
+            this.col = col;
+        }
+
+        
+        
+    }
+}
